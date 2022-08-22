8d3c0bf96b37dc79b4de757be5a24e66024af751
Tetiana <tetmyslynska@gmail.com>
2022-08-22 12:53:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import AppRoutes from './routes/AppRoutes';
+import AppNav from './components/AppNav';
+import {useState} from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+  axios.defaults.headers.common['Authorization'] = 'Bearer' + (user ? user.jw_token : "");
+
+  return (
+    <div className="App">
+      <AppNav user={user} setUser = {setUser}/>
+      <AppRoutes user={user} setUser = {setUser}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,68 @@
+import './Home.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import Post from '../components/Post';
+import AddPost from '../components/AddPost';
+
+
+
+const Home = (props)=> {
+    const [posts, setPosts] = useState([])
+
+    const getLatestPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/latest")
+        .then((res)=> {
+            setPosts(res.data)
+        })
+        .catch((error)=>{
+            console.error(error);
+        });
+    
+    } 
+
+    const getNextPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/older-then", {
+            date: posts[posts.length - 1].created_at
+        })
+        .then((res)=> {
+            setPosts(posts.concat(res.data));
+        })
+        .catch((error)=>{
+            console.error(error);
+        });
+    
+    } 
+
+    const getPrevPosts = () => {
+        axios.post("https://akademia108.pl/api/social-app/post/newer-then", {
+            date: posts[0].created_at
+        })
+        .then((res)=> {
+            setPosts(res.data.concat(posts));
+        })
+        .catch((error)=>{
+            console.error(error);
+        });
+    
+    } 
+
+ useEffect(()=>{
+        getLatestPosts();
+    }, [props.user]);
+
+
+    return (
+        <div className="home">
+            {props.user && <AddPost getPrevPosts={getPrevPosts}/>}
+            <div className="postList">
+                    {posts.map((post)=> {
+                return <Post post={post} key={post.id}/>;
+                        })}
+                <button className="btn loadMore" onClick={getNextPosts}>Load more</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
