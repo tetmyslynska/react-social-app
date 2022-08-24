9789980f888530f6035e4d65e654359436a8e7c8
Tetiana <tetmyslynska@gmail.com>
2022-08-24 20:13:52 +0200

    follow-unfollow users

--- src/components/Post.js
+++ src/components/Post.js
@@ -45,6 +45,18 @@ const Post = (props) => {
       });
   };
 
+  const unfollow = (id) => {
+    axios.post('https://akademia108.pl/api/social-app/follows/disfollow', {
+       leader_id: id
+    })
+    .then(()=>{
+        props.getLatestPosts();
+    })
+    .catch((error) => {
+        console.error(error);
+      });
+}
+
   return (
     <div className="post">
       <div className="avatar">
@@ -62,6 +74,8 @@ const Post = (props) => {
               Delete
             </button>
           )}
+
+          {props.user && props.user.username !== props.post.user.username && (<button className='btn' onClick={()=>unfollow(props.post.user.id)}>Unfollow</button>)}
 
           {props.user && (
             <button
